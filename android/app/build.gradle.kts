08e921d0a3c7121378f8a9da6fc52d460a2bd70c
INFIXIFY <sarthaksinghyadav1@gmail.com>
2026-09-24 11:36:39 +0000

    Disable R8 minification to fix WorkManager crash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,47 +1,55 @@
 plugins {
-        id("com.android.application")
-            id("dev.flutter.flutter-gradle-plugin")
-                id("com.google.gms.google-services")
+            id("com.android.application")
+                id("dev.flutter.flutter-gradle-plugin")
+                    id("com.google.gms.google-services")
 }
 
 android {
-        namespace = "com.example.reward_app_flutter"
-            compileSdk = flutter.compileSdkVersion
-                ndkVersion = flutter.ndkVersion
+            namespace = "com.example.reward_app_flutter"
+                compileSdk = flutter.compileSdkVersion
+                    ndkVersion = flutter.ndkVersion
 
-                    compileOptions {
-                                sourceCompatibility = JavaVersion.VERSION_17
-                                        targetCompatibility = JavaVersion.VERSION_17
-                    }
-
-                        defaultConfig {
-                                    applicationId = "com.example.reward_app_flutter"
-                                            minSdk = 24
-                                                    targetSdk = flutter.targetSdkVersion
-                                                            versionCode = flutter.versionCode
-                                                                    versionName = flutter.versionName
-                                                                            multiDexEnabled = true
+                        compileOptions {
+                                        sourceCompatibility = JavaVersion.VERSION_17
+                                                targetCompatibility = JavaVersion.VERSION_17
                         }
 
-                            buildTypes {
-                                        release {
-                                                        // Abhi debug signing use ho rahi hai taaki missing-keystore
-                                                                    // error na aaye aur APK reliably build ho.
-                                                                                // Play Store release ke waqt apna khud ka upload keystore
-                                                                                            // banakar yahan use karein.
-                                                                                                        signingConfig = signingConfigs.getByName("debug")
-                                        }
+                            defaultConfig {
+                                        applicationId = "com.example.reward_app_flutter"
+                                                minSdk = 24
+                                                        targetSdk = flutter.targetSdkVersion
+                                                                versionCode = flutter.versionCode
+                                                                        versionName = flutter.versionName
+                                                                                multiDexEnabled = true
                             }
 
-                                kotlin {
-                                            compilerOptions {
-                                                            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-                                            }
+                                buildTypes {
+                                                release {
+                                                                    // R8 minification OFF - yeh WorkManager/WorkDatabase crash fix karta hai
+                                                                                // (androidx.work ki reflection-based classes R8 se toot rahi thi).
+                                                                                            isMinifyEnabled = false
+                                                                                                        isShrinkResources = false
+
+                                                                                                                    // Abhi debug signing use ho rahi hai taaki missing-keystore
+                                                                                                                                // error na aaye aur APK reliably build ho.
+                                                                                                                                            // Play Store release ke waqt apna khud ka upload keystore
+                                                                                                                                                        // banakar yahan use karein.
+                                                                                                                                                                    signingConfig = signingConfigs.getByName("debug")
+                                                }
                                 }
+
+                                    kotlin {
+                                                compilerOptions {
+                                                                    jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                                                }
+                                    }
 }
 
 // IMPORTANT: yeh block android{} ke BAHAR hona chahiye, andar nahi.
 flutter {
-        source = "../.."
+            source = "../.."
 }
 
+                                                }
+                        }
+}
