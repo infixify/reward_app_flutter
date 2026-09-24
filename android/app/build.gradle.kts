9f8a4322ff9083e15fa4bcd3aa75f224311d336b
INFIXIFY <sarthaksinghyadav1@gmail.com>
2026-09-24 21:27:32 +0000

    Lock permanent debug keystore for consistent SHA-1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
         multiDexEnabled = true
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
