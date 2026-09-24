8ac63897bacd76ca74266b4a95804a73a0cb0c8d
INFIXIFY <sarthaksinghyadav1@gmail.com>
2026-09-24 11:43:18 +0000

    Fix build.gradle.kts syntax + disable R8 minification

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,55 +1,43 @@
 plugins {
-            id("com.android.application")
-                id("dev.flutter.flutter-gradle-plugin")
-                    id("com.google.gms.google-services")
+    id("com.android.application")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-            namespace = "com.example.reward_app_flutter"
-                compileSdk = flutter.compileSdkVersion
-                    ndkVersion = flutter.ndkVersion
+    namespace = "com.example.reward_app_flutter"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
-                        compileOptions {
-                                        sourceCompatibility = JavaVersion.VERSION_17
-                                                targetCompatibility = JavaVersion.VERSION_17
-                        }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
-                            defaultConfig {
-                                        applicationId = "com.example.reward_app_flutter"
-                                                minSdk = 24
-                                                        targetSdk = flutter.targetSdkVersion
-                                                                versionCode = flutter.versionCode
-                                                                        versionName = flutter.versionName
-                                                                                multiDexEnabled = true
-                            }
+    defaultConfig {
+        applicationId = "com.example.reward_app_flutter"
+        minSdk = 24
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+    }
 
-                                buildTypes {
-                                                release {
-                                                                    // R8 minification OFF - yeh WorkManager/WorkDatabase crash fix karta hai
-                                                                                // (androidx.work ki reflection-based classes R8 se toot rahi thi).
-                                                                                            isMinifyEnabled = false
-                                                                                                        isShrinkResources = false
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 
-                                                                                                                    // Abhi debug signing use ho rahi hai taaki missing-keystore
-                                                                                                                                // error na aaye aur APK reliably build ho.
-                                                                                                                                            // Play Store release ke waqt apna khud ka upload keystore
-                                                                                                                                                        // banakar yahan use karein.
-                                                                                                                                                                    signingConfig = signingConfigs.getByName("debug")
-                                                }
-                                }
-
-                                    kotlin {
-                                                compilerOptions {
-                                                                    jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-                                                }
-                                    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        }
+    }
 }
 
-// IMPORTANT: yeh block android{} ke BAHAR hona chahiye, andar nahi.
 flutter {
-            source = "../.."
-}
-
-                                                }
-                        }
+    source = "../.."
 }
